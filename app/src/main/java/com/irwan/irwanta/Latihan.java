1fe0b21df0a5447a7303bfbaf9134a6ddc677a62
a <a@a.a>
2021-07-23 17:38:27 +0800

    gif

--- app/src/main/java/com/irwan/irwanta/Latihan.java
+++ app/src/main/java/com/irwan/irwanta/Latihan.java
@@ -2,9 +2,11 @@ package com.irwan.irwanta;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.irwan.irwanta.R;
 import com.irwan.irwanta.Signature;
 import com.irwan.irwanta.Splash;
@@ -14,8 +16,10 @@ public class Latihan extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tulis);
-        GifImageView gifImageView = (GifImageView) findViewById(R.id.GifImageView);
-        gifImageView.setGifImageResource(R.mipmap.ja1);
+//        GifImageView gifImageView = (GifImageView) findViewById(R.id.GifImageView);
+//        gifImageView.setGifImageResource(R.mipmap.ja1);
+        ImageView imageView = findViewById(R.id.gambar);
+        Glide.with(this).load(R.mipmap.ba1).into(imageView);
     }
 
 
