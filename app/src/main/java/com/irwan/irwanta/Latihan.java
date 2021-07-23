88fe3ca4a13aff005ac16030bab78a6d8205cb56
a <a@a.a>
2021-07-23 17:48:56 +0800

    latihan

--- app/src/main/java/com/irwan/irwanta/Latihan.java
+++ app/src/main/java/com/irwan/irwanta/Latihan.java
@@ -16,8 +16,6 @@ public class Latihan extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tulis);
-//        GifImageView gifImageView = (GifImageView) findViewById(R.id.GifImageView);
-//        gifImageView.setGifImageResource(R.mipmap.ja1);
         ImageView imageView = findViewById(R.id.gambar);
         Glide.with(this).load(R.mipmap.ba1).into(imageView);
     }
